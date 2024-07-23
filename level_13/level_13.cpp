ce476679e1eb6ae2e5f661d44b561a11760e3b1c
zhenhua32 <zhenhua32@gmail.com>
2024-07-23 21:29:10 +0800

    save

---
+++ level_13/level_13.cpp
@@ -0,0 +1,42 @@
+﻿// level_13.cpp: 定义应用程序的入口点。
+//
+
+#include "level_13.h"
+
+using namespace std;
+
+template<typename T>
+T add(T i, T j) {
+	return i + j;
+}
+
+template<int a, int b>
+int addv2() {
+	return a + b;
+}
+
+template<typename T, int a>
+T addv3(T i) {
+	return i + a;
+}
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	int a = 10;
+	int b = 12;
+	int c = add(a, b);
+	cout << "a + b = " << c << endl;
+
+	float d = add(1.0, 2.3);
+	cout << "1.0 + 2.3 = " << d << endl;
+
+	int e = addv2<1, 2>();
+	cout << "1 + 2 = " << e << endl;
+
+	float f = addv3<float, 3>(1.0);
+	cout << "1.0 + 3 = " << f << endl;
+
+	return 0;
+}
---
+++ level_13/level_13.h
@@ -0,0 +1,48 @@
+﻿// level_13.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+
+// TODO: 在此处引用程序需要的其他标头。
+
+#ifndef __MY__
+#define __MY__
+
+template<typename T>
+class myvector {
+public:
+	typedef T* myiterator;  // 定义类型别名
+
+	myvector();
+	myvector& operator=(const myvector&);
+
+	myiterator mybegin();
+	myiterator myend();
+
+	// 成员函数声明加实现
+	void myfunc() {};
+	// 成员函数仅声明
+	void myfunc2();
+};
+
+// 定义在外面的成员函数的实现
+template<typename T>
+void myvector<T>::myfunc2() {
+	std::cout << "myfunc2" << std::endl;
+}
+
+template<typename T>
+myvector<T>::myvector() {
+	std::cout << "myvector的构造函数" << std::endl;
+}
+
+template<typename T>
+myvector<T>& myvector<T>::operator=(const myvector& v) {
+	std::cout << "myvector的赋值运算符" << std::endl;
+	return *this;
+}
+
+#endif // !__MY__
+
